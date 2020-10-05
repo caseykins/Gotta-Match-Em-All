55454dff380269fcecdd6a49c7c7b17345ce52b7
Caseykins <caseyjenkins92@gmail.com>
2020-10-05 17:11:17 -0600

    Added button and basic functionality

--- Game/mems.js
+++ Game/mems.js
@@ -1,21 +1,39 @@
 console.log('Remember, remember..')
 
 
+let btnContainer = document.createElement('div')
+btnContainer.setAttribute('class', 'button')
+document.body.appendChild(btnContainer)
 
 let container = document.createElement('div')
-container.setAttribute('class', 'container')
+container.setAttribute('class', 'cards')
 document.body.appendChild(container)
 
+let shuffleButton = document.createElement('button')
+shuffleButton.setAttribute('id', 'button')
+shuffleButton.innerText = "Get Cards"
+btnContainer.appendChild(shuffleButton)
 
+let timer = 45
+let matches = 0
 
 // create the deck
-const createCards = () => {
+const getCards = () => {
+    shuffleButton.addEventListener('click', function() {
+    disableButton()
     for (let i=0; i < 30; i++) {
     let cards = document.createElement('img')
     cards.setAttribute('class', 'cards')
     cards.setAttribute('src', './images/back.png')
     container.appendChild(cards)
-    }
+        }
+    })
 }
 
-createCards()
+const disableButton = () => {
+    shuffleButton.removeEventListener('click', function() {
+        alert('You have already clicked this.')
+    })
+}
+
+getCards()
