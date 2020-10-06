3989446d8fe4e5023711c4024329e93d8c622525
Caseykins <caseyjenkins92@gmail.com>
2020-10-06 17:05:52 -0600

    added flipCards method - does not work

--- Game/mems.js
+++ Game/mems.js
@@ -5,19 +5,18 @@ let btnContainer = document.createElement('div')
 btnContainer.setAttribute('class', 'button')
 document.body.appendChild(btnContainer)
 
+let cardsContainer = document.createElement('div')
+cardsContainer.setAttribute('class', 'cardsContainer')
+document.body.appendChild(cardsContainer)
+
 let cards = document.createElement('img')
 cards.setAttribute('class', 'cards')
 cards.setAttribute('src', './images/back.png')
 
-let container = document.createElement('div')
-container.setAttribute('class', 'cards')
-document.body.appendChild(container)
-
-
-
-
-
-
+let cardFronts = document.createElement('img')
+// cardsFronts.setAttribute('class', 'cardFronts')
+// cardsFronts.setAttribute('src', `./images/card${i}.png`)
+// cardsFronts.setAttribute('value', i)
 
 
 // create the deck
@@ -26,7 +25,7 @@ let getCards = () => {
         let cards = document.createElement('img')
         cards.setAttribute('class', 'cards')
         cards.setAttribute('src', './images/back.png')
-        container.appendChild(cards)
+        cardsContainer.appendChild(cards)
         // allow button 
         cardsButton.removeEventListener('click', getCards)
         }
@@ -40,35 +39,45 @@ let getCards = () => {
 
 
 // currently not working
- const randomizeCards = () => {
-     let randomOrder = Math.floor(Math.random() * 20)
-     cards.style.order = randomOrder
+//const randomizeCards = () => {
+//     let randomOrder = Math.floor(Math.random() * 20)
+//     cards.style.order = randomOrder
 
     //  let cardFronts = document.createElement('img')
     //  cardFronts.setAttribute('class', 'cardfronts')
     //  cardFronts.setAttribute('src', `./images/card${i}.png`)
-     }
+//    }
 
 
 
-    //                                 NEED TO DETERMINE WHERE CARD FRONTS GO (randomize, get cards, reveal cards?)
+    //                     NEED TO DETERMINE WHERE CARD FRONTS GO (randomize, get cards, reveal cards?)
 
-// const revealCards = () => {
-
+/*const revealCards = () => {
+    cards.addEventListener('click', flipCards)
+    let cards = document.createElement('img')
+    cards.setAttribute('id', 'cardFronts')
+    cards.setAttribute('src', `./images/card${i}.png`)
 // select card 1
-
 // select card 2
-
 // if match: leave overturned and disable
-
 // if not a match: reset cards to downward position
-
-
-
 // }
-//}
+}*/
+
+const flipCards = () => {
+    for (let i=0; i < 20; i++)
+    cardFronts = document.createElement('img')
+    cardsFronts.setAttribute('class', 'cardFronts')
+    cardsFronts.setAttribute('src', `./images/card${i}.png`)
+    cardsFronts.setAttribute('value', i)
+}
+
+let showCard = cardsContainer.querySelectorAll('cards')
+cards.addEventListener('click', flipCards)
+
 
 
 //getCards()
 //randomizeCards()
+flipCards()
 
